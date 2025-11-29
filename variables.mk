959cec1493aa7a084834ba7f51ce490cf19a3691
ichmy55 <ichmy@nifty.com>
2025-11-29 17:00:02 +0900

    2025-11-29-2

---
+++ variables.mk
@@ -0,0 +1,10 @@
+#
+# Docker コマンドマクロ
+#
+DOCKER_IMAGE  := ghcr.io/ichmy55/opencae-slides/texcomp:main
+DOCKER_NAME   := opencae-slides
+PACKAGE_USE   := 1              # 標準では出来合いパッケージを使用
+#
+# 作成するスライド名
+#
+DEST_PDF := opencae-kantou-s-028
