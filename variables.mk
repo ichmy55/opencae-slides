9e05e98c9b80379de7f2afb89c5d7dd10d139284
ichmy55 <ichmy@nifty.com>
2025-12-27 19:03:55 +0900

    2025-12-207-04

--- variables.mk
+++ variables.mk
@@ -1,9 +1,9 @@
 #
 # Docker コマンドマクロ
 #
-DOCKER_IMAGE  := ghcr.io/ichmy55/opencae-slides/texcomp:main
-DOCKER_NAME   := opencae-slides
-PACKAGE_USE   := 1              # 標準では出来合いパッケージを使用
+DOCKER_IMAGE  := ghcr.io/ichmy55/beamer-example/texcomp:main
+DOCKER_NAME   := beamer-example
+PACKAGE_USE   := 1              # 出来合いパッケージを使用するには"1"をセット
 #
 # 作成するスライド名
 #
